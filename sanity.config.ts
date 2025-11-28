3fefff6cf2bc267f280e5838ed3398922c942172
Paul E. Long <paulelong@outlook.com>
2025-11-28 11:09:12 -0500

    feat: update Sanity configuration to use custom schemas and adjust structure titles for Studio Timeline

--- sanity.config.ts
+++ sanity.config.ts
@@ -10,7 +10,8 @@ import {structureTool} from 'sanity/structure'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './src/sanity/env'
-import {schema} from './src/sanity/schemaTypes'
+// Use custom Timeline/Room/Doc schemas
+import {schemaTypes} from './studio/schemas'
 import {structure} from './src/sanity/structure'
 
 export default defineConfig({
@@ -18,7 +19,7 @@ export default defineConfig({
   projectId,
   dataset,
   // Add and edit the content schema in the './sanity/schemaTypes' folder
-  schema,
+  schema: { types: schemaTypes },
   plugins: [
     structureTool({structure}),
     // Vision is for querying with GROQ from inside the Studio
